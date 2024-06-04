8357e6ca094175f07fcf4ec2a584af6c6c90996c
Ika Yébenes Herce <ikayherce@gmail.com>
2024-06-04 19:48:26 +0000

    add bootstrap template and add home page html

--- finest_cupcakes/settings.py
+++ finest_cupcakes/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = ['static/']
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
