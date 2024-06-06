5c590f183a43189705a0dc54008bde56d0e28d66
Ika Yébenes Herce <ikayherce@gmail.com>
2024-06-06 20:54:41 +0000

    add register form code

--- shop/views.py
+++ shop/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 from django import forms
 
 def home(request):
@@ -35,4 +36,21 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request,'register.html',{})
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #login user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request,("You have registered successfully"))
+            return redirect('home')
+        else: 
+            messages.success(request,("Oops,there was a problem registering, please try again"))
+            return redirect('register')
+
+    else:
+        return render(request,'register.html',{'form':form})
