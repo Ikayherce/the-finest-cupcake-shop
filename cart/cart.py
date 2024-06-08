1d7b4fce1d52aad0101e6fb18b0933019452fe31
Ika Yébenes Herce <ikayherce@gmail.com>
2024-06-08 10:10:52 +0000

    correct indentation on cart.py

--- cart/cart.py
+++ cart/cart.py
@@ -1,13 +1,14 @@
+
 class Cart():
     def __init__(self,request):
         self.session = request.session
 
     #Get the current session key if it exists
-    cart = self.session.get('session_key')
+        cart = self.session.get('session_key')
 
     #If the user is new there is no session key, we create one
-    if 'session_key' not in request.session:
-        cart=self.session['session_key'] = {}
+        if 'session_key' not in request.session:
+            cart = self.session['session_key'] = {}
 
     #make sure cart is available on all pages
-    self.cart = cart
+        self.cart = cart
