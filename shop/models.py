e97557b88e20f3bb01e9d67ff8d55580c744200d
Ika Yébenes Herce <ikayherce@gmail.com>
2024-06-10 21:07:07 +0000

    add about text, correct product model to temporarily fix category bug

--- shop/models.py
+++ shop/models.py
@@ -27,7 +27,7 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
-    #category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     categories = models.ManyToManyField(Category, related_name='products')
     description = models.CharField(max_length=250, default='',blank=True, null= True)
     image = models.ImageField(upload_to='uploads/product/')
