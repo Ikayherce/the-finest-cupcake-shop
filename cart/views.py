45d133d5c05912e9ad019cd58d833b8e18b44e97
Ika Yébenes Herce <ikayherce@gmail.com>
2024-06-12 18:12:48 +0000

    add product added to cart message

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from.cart import Cart
 from shop.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
 
 
 def cart_summary(request):
@@ -31,6 +32,7 @@ def cart_add(request):
 
         #response = JsonResponse({'Product Name: ':product.name})
         response = JsonResponse({'qty': cart_quantity})
+        messages.success(request, ("Product added to cart"))
         return response
 
 def cart_delete(request):
