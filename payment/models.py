abad3b3a28d0f89b99b448be9d6626b5c81b4711
Ika Yébenes Herce <ikayherce@gmail.com>
2024-06-13 18:43:54 +0000

    add shipping address model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ShippingAddress(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+	shipping_full_name = models.CharField(max_length=255)
+	shipping_email = models.CharField(max_length=255)
+	shipping_address1 = models.CharField(max_length=255)
+	shipping_address2 = models.CharField(max_length=255, null=True, blank=True)
+	shipping_city = models.CharField(max_length=255)
+	shipping_zipcode = models.CharField(max_length=255, null=True, blank=True)
+	shipping_country = models.CharField(max_length=255)
+
+	# Don't pluralize address
+	class Meta:
+		verbose_name_plural = "Shipping Address"
+
+	def __str__(self):
+		return f'Shipping Address - {str(self.id)}'
+
+# Create a user Shipping Address by default when user signs up
+def create_shipping(sender, instance, created, **kwargs):
+	if created:
+		user_shipping = ShippingAddress(user=instance)
+		user_shipping.save()
+
+# Automate the profile thing
+post_save.connect(create_shipping, sender=User)
