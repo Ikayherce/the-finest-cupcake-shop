6eb396494e8c1c737e36ab787ad2c9296284f1ef
Ika Yébenes Herce <ikayherce@gmail.com>
2024-06-13 15:44:27 +0000

    add logic for logged in users so their cart persists after logging out and back in

--- cart/cart.py
+++ cart/cart.py
@@ -1,18 +1,19 @@
-from shop.models import Product
+from shop.models import Product, Profile
 
 
 class Cart():
     def __init__(self,request):
         self.session = request.session
-
-    #Get the current session key if it exists
+        #get request
+        self.request = request
+        #Get the current session key if it exists
         cart = self.session.get('session_key')
 
-    #If the user is new there is no session key, we create one
+        #If the user is new there is no session key, we create one
         if 'session_key' not in request.session:
             cart = self.session['session_key'] = {}
 
-    #make sure cart is available on all pages
+        #make sure cart is available on all pages
         self.cart = cart
 
     def add(self, product, quantity): 
@@ -26,6 +27,18 @@ class Cart():
             self.cart[product_id] = int(product_qty)
 
         self.session.modified = True 
+
+        # Deal with logged in user
+        if self.request.user.is_authenticated:
+		    # Get the current user profile
+            current_user = Profile.objects.filter(user__id=self.request.user.id)
+		    # Convert {'3':1, '2':4} to {"3":1, "2":4}
+            carty = str(self.cart)
+            carty = carty.replace("\'", "\"")
+		    # Save carty to the Profile Model
+            current_user.update(old_cart=str(carty))
+
+
 
     def cart_total(self):
         # Get product IDs
