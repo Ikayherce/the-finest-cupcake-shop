d45e280269937a6bc498ba5dd12e014e1a23b243
Ika Yébenes Herce <ikayherce@gmail.com>
2024-06-13 11:57:07 +0000

    add update profile info template and functionality

--- shop/views.py
+++ shop/views.py
@@ -1,38 +1,35 @@
 from django.shortcuts import render, redirect
-from .models import Product, Category
+from .models import Product, Category, Profile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 from django import forms
 from django.http import Http404
 
 
 def update_info(request):
-	if request.user.is_authenticated:
-		# Get Current User
-		current_user = Profile.objects.get(user__id=request.user.id)
-		# Get Current User's Shipping Info
-		shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
-		
-		# Get original User Form
-		form = UserInfoForm(request.POST or None, instance=current_user)
-		# Get User's Shipping Form
-		shipping_form = ShippingForm(request.POST or None, instance=shipping_user)		
-		if form.is_valid() or shipping_form.is_valid():
-			# Save original form
-			form.save()
-			# Save shipping form
-			shipping_form.save()
+    if request.user.is_authenticated:
+        # Get Current User's Profile
+        try:
+            current_user = Profile.objects.get(user__id=request.user.id)
+        except Profile.DoesNotExist:
+            current_user = Profile.objects.create(user=request.user)
+        
+        # Get original User Form
+        form = UserInfoForm(request.POST or None, instance=current_user)
 
-			messages.success(request, "Your Info Has Been Updated!!")
-			return redirect('home')
-		return render(request, "update_info.html", {'form':form, 'shipping_form':shipping_form})
-	else:
-		messages.success(request, "You Must Be Logged In To Access That Page!!")
-		return redirect('home')
-
+        if form.is_valid():
+            # Save original form
+            form.save()
+            messages.success(request, "Your Details Have Been Updated")
+            return redirect('home')
+        
+        return render(request, 'update_info.html', {'form': form})
+    else:
+        messages.error(request, "You Must Be Logged In To Access That Page!")
+        return redirect('home')
 
 
 def update_password(request):
