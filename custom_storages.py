ea2546416dcf1fee85c1927811dbb0455decb3d0
Ika Yébenes Herce <ikayherce@gmail.com>
2024-06-15 19:04:10 +0000

    add logging to custom storages

--- custom_storages.py
+++ custom_storages.py
@@ -1,3 +1,4 @@
+import logging
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
@@ -6,3 +7,10 @@ class StaticStorage(S3Boto3Storage):
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
+
+try:
+    AWS_STORAGE_BUCKET_NAME = settings.AWS_STORAGE_BUCKET_NAME
+    AWS_S3_REGION_NAME = settings.AWS_S3_REGION_NAME
+    logger.info(f"AWS S3 storage configured for bucket '{AWS_STORAGE_BUCKET_NAME}'")
+except AttributeError as e:
+    logger.error(f"AWS settings are not properly configured: {e}")
