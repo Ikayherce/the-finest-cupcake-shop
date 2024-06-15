7451ac0c750877800f48ce645ac8faab913e4f9f
Ika Yébenes Herce <ikayherce@gmail.com>
2024-06-15 18:22:03 +0000

    update settings for deployment

--- finest_cupcakes/settings.py
+++ finest_cupcakes/settings.py
@@ -141,7 +141,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     #bucket configuration
     AWS_STORAGE_BUCKET_NAME = 'the-finest-cupcake-shop-1'
-    AWS_S3_REGION_NAME = 'Europe (Stockholm) eu-north-1'
+    AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
