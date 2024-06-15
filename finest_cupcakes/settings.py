c4f2a7143036f4a66e31a376734c458dbb9f79b0
Ika Yébenes Herce <ikayherce@gmail.com>
2024-06-15 16:07:53 +0000

    add settings for deployment to heroku

--- finest_cupcakes/settings.py
+++ finest_cupcakes/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 
 
@@ -28,7 +30,7 @@ SECRET_KEY = 'django-insecure-g!14bl0$d8-am-2rc2(@fpum_=a4b42oiw+9*ic48_x&ugjcj4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ikayherce-thefinestcupc-8bp0zv1nthe.ws-eu114.gitpod.io', '8000-ikayherce-thefinestcupc-od1yo7fimsf.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-ikayherce-thefinestcupc-od1yo7fimsf.ws.codeinstitute-ide.net', 'the-finest-cupcake-shop.herokuapp.com']
 
 
 # Application definition
@@ -80,13 +82,17 @@ WSGI_APPLICATION = 'finest_cupcakes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+            'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
